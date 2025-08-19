232905dae52eaae627b6927c40c3f6d4afe6e582
EleazarDev <eleazarjarquincv@gmail.com>
2025-08-19 11:45:36 -0600

    start: Initial commit

---
+++ src/main/java/com/unsis/data_unsis_backend/DataUnsisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unsis.data_unsis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUnsisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUnsisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsis/data_unsis_backend/DataUnsisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsis.data_unsis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUnsisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
